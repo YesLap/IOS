f2a2d9c9dad02d4d51ffe6b5b0df4ba16508da0d
Bruno <brunoteixeiralc@gmail.com>
2017-10-06 20:47:46 -0300

    [yeslap] - sign in screen

--- yeslap/ViewController.swift
+++ yeslap/ViewController.swift
@@ -12,14 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension UITextField{
+    @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        set {
+            self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: newValue!])
+        }
     }
-
-
 }
 
