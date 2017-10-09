420c7b67b9a1dd7806a974689f72f143b875004b
Bruno Lemgruber <brunoteixeiralc@gmail.com>
2017-10-09 14:33:11 -0300

    [yesLap] - register new user and profile layouts

---
+++ yeslap/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  yeslap
+//
+//  Created by Bruno Lemgruber on 09/10/2017.
+//  Copyright © 2017 Bruno Lemgruber. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var imgUser: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.imgUser.layer.cornerRadius = self.imgUser.bounds.width/2.0
+        self.imgUser.layer.masksToBounds = true
+    }
+    
+    
+}
